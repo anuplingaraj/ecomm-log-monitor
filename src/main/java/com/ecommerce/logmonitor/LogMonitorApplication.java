58d0500d3e4c931b990e64f2cd2aeb4e39500d6d
akshayjp123 <akshayjpatil11@gmail.com>
2019-09-24 15:40:57 -0400

    first commit

---
+++ src/main/java/com/ecommerce/logmonitor/LogMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.logmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogMonitorApplication.class, args);
+	}
+
+}
